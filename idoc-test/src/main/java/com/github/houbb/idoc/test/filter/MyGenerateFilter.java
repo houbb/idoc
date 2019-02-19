a2d02b068717235c7ac6a2e49ce05e8cab176c1d
houbb <1060732496@qq.com>
2019-02-20 00:03:12 +0800

    release branch 0.0.1

--- idoc-test/src/main/java/com/github/houbb/idoc/test/filter/MyGenerateFilter.java
+++ idoc-test/src/main/java/com/github/houbb/idoc/test/filter/MyGenerateFilter.java
@@ -1,20 +1,20 @@
-package com.github.houbb.idoc.test.filter;
-
-import com.github.houbb.idoc.api.core.filter.IDocGenerateFilter;
-import com.github.houbb.idoc.api.model.metadata.DocClass;
-
-/**
- * @author binbin.hou
- * @since 0.0.1
- */
-public class MyGenerateFilter implements IDocGenerateFilter {
-
-    @Override
-    public boolean include(DocClass docClass) {
-        if("QueryUserService".equalsIgnoreCase(docClass.getName())) {
-            return true;
-        }
-        return false;
-    }
-
-}
+//package com.github.houbb.idoc.test.filter;
+//
+//import com.github.houbb.idoc.api.core.filter.IDocGenerateFilter;
+//import com.github.houbb.idoc.api.model.metadata.DocClass;
+//
+///**
+// * @author binbin.hou
+// * @since 0.0.1
+// */
+//public class MyGenerateFilter implements IDocGenerateFilter {
+//
+//    @Override
+//    public boolean include(DocClass docClass) {
+//        if("QueryUserService".equalsIgnoreCase(docClass.getName())) {
+//            return true;
+//        }
+//        return false;
+//    }
+//
+//}
